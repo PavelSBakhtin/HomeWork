33253b0684f0b3b77e3fc99e16cafcbcc48d9caf
PavelSB <PavelS@mail.ru>
2022-09-18 02:55:07 +0300

    lesson 1 task 6 version 2 done

---
+++ L1T6V2/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = -3;
+
+Console.WriteLine("We have: " + a);
+Console.WriteLine("This number is even ?");
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("YES");
+}
+if (a % 2 != 0)
+{
+    Console.WriteLine("NO");
+}
