eef6b9afefb4fc237cac402c2039c034f3ea8f5f
PavelSB <PavelS@mail.ru>
2022-09-18 02:41:38 +0300

    lesson 1 task 4 version 2 done

---
+++ L1T4V2/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 44;
+int b = 5;
+int c = 78;
+int max = a;
+
+Console.WriteLine("We have: " + a + " and " + b + " and " + c);
+Console.WriteLine("Which of these max ?");
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine(max);
