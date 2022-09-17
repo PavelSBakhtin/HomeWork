fc901b45a462156c90044464eab1a545120c77dc
PavelSB <PavelS@mail.ru>
2022-09-18 02:06:00 +0300

    lesson 1 task 2 version 1 done

---
+++ L1T2V1/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 5;
+int b = 7;
+
+Console.WriteLine("We have: " + a + " and " + b);
+Console.WriteLine("Which of these max ?");
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
