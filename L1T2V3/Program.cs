6b2acc5ffbf441123895a834ae9d28322e4ee46b
PavelSB <PavelS@mail.ru>
2022-09-18 02:20:44 +0300

    lesson 1 task 2 version 2 done

---
+++ L1T2V3/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = -9;
+int b = -3;
+
+Console.WriteLine("We have: " + a + " and " + b);
+Console.WriteLine("Which of these max ?");
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
