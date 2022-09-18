09661c8b6c2848c0b5ef038a28537e8ffe7603c3
PavelSB <PavelS@mail.ru>
2022-09-18 11:15:46 +0300

    main structure lesson 1 task 1 added

--- lesson1_task1/Program.cs
+++ lesson1_task1/Program.cs
@@ -1,5 +1,24 @@
 ﻿// Console.WriteLine("Введите число a");
 // int a = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите число a");
-int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Вы попали в игру - Угадай число!");
+int secret_number = 5;
+while (true)
+{
+    Console.WriteLine("Введите число от 1 до 9");
+    int user_number = int.Parse(Console.ReadLine());
+    Console.WriteLine(user_number);
+    if (user_number > secret_number)
+    {
+        Console.WriteLine("Секретное число меньше");
+    }
+    else if (user_number < secret_number)
+    {
+        Console.WriteLine("Секретное число больше");
+    }
+    else if (user_number == secret_number)
+    {
+        Console.WriteLine("Вы угадали секретное число!");
+    }
+}
+
