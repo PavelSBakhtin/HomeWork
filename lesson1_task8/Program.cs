72e00b2e162b5e08d3b09c6fe1b417594902dd14
PavelSB <PavelS@mail.ru>
2022-09-18 23:03:54 +0300

    lesson 1 task 8 done

---
+++ lesson1_task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите любое целое число");
+int user_number = int.Parse(Console.ReadLine());
+int x = 1;
+
+Console.WriteLine("Все чётные числа от 1 до " + user_number + ":");
+while (x < user_number)
+{
+    if (x % 2 == 0)
+    {
+        Console.WriteLine(x);
+    }
+    x++;
+}
+// Console.WriteLine(x += 2); - или эта строка вместо алгоритма IF, но тогда int x = 0 (подсказка из мануала)
