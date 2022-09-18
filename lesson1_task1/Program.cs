8659216255abd24b24e1627093e239f18f29171c
PavelSB <PavelS@mail.ru>
2022-09-18 11:51:54 +0300

    lesson 1 task 1 done

--- lesson1_task1/Program.cs
+++ lesson1_task1/Program.cs
@@ -1,24 +1,39 @@
-﻿// Console.WriteLine("Введите число a");
-// int a = int.Parse(Console.ReadLine());
-
+﻿int secret_number = new Random().Next(1, 10);
+int attempt = 3;
 Console.WriteLine("Вы попали в игру - Угадай число!");
-int secret_number = 5;
-while (true)
+Console.WriteLine("У вас есть " + attempt + " попытки");
+bool Win = false;
+
+while (attempt > 0)
 {
     Console.WriteLine("Введите число от 1 до 9");
     int user_number = int.Parse(Console.ReadLine());
-    Console.WriteLine(user_number);
+    // Console.WriteLine(user_number);
     if (user_number > secret_number)
     {
         Console.WriteLine("Секретное число меньше");
+        attempt--;
     }
     else if (user_number < secret_number)
     {
         Console.WriteLine("Секретное число больше");
+        attempt--;
     }
     else if (user_number == secret_number)
     {
         Console.WriteLine("Вы угадали секретное число!");
+        Win = true;
+        break;
     }
+    Console.WriteLine("Осталось попыток: " + attempt);
 }
-
+if (Win)
+{
+    Console.WriteLine("Вы выйграли! Приходите снова :)");
+    Console.WriteLine("Осталось попыток " + (attempt-1));
+}
+else
+{
+    Console.WriteLine("Вы проиграли :( Секретное число было: " + secret_number);
+    Console.WriteLine("Попробуйте ещё раз :)");
+}
