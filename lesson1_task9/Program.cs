5ac5ee797f5446372623cf53c60f0157df6992e7
PavelSB <PavelS@mail.ru>
2022-09-24 01:29:36 +0300

    lesson 1 task 9 added

---
+++ lesson1_task9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Функции:
+// void   string   int   bool   ...
+
+void Function1()
+{
+    Console.WriteLine(1);
+}
+void Function2()
+{
+    Console.WriteLine(2);
+}
+void Function3()
+{
+    Console.WriteLine(3);
+}
+Function2();
