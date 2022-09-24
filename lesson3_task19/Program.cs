083d5d116bfc4a54b217f24f1dafb3cc8555333d
PavelSB <PavelS@mail.ru>
2022-09-24 05:15:15 +0300

    lesson 3 task 19 done

---
+++ lesson3_task19/Program.cs
@@ -0,0 +1,30 @@
+﻿int NumberOne(int num, int NumberOfFirstNumbers)
+{
+    while (num / Math.Pow(10, NumberOfFirstNumbers) > 1)
+    {
+        num /= 10;
+    }
+    return num;
+}
+
+Console.WriteLine("enter a five-digit number:");
+int number = int.Parse(Console.ReadLine());
+if (number < 0) number *= -1;
+if (number > 9999 && number < 100000)
+{
+    int x = number % 100;
+    int y = NumberOne(number, 2);
+    
+    if (x / 10 == y % 10 && x % 10 == y / 10)
+    {
+        Console.WriteLine("yes, the number is a palindrome");
+    }
+    else
+    {
+        Console.WriteLine("no, the number is not a palindrome");
+    }
+}
+else
+{
+    Console.WriteLine("you entered the wrong number");
+}
