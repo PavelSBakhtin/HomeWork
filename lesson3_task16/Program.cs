ed49dfe49f39075c0c733e1de1b53a9afd1b8e5b
PavelSB <PavelS@mail.ru>
2022-09-24 04:35:25 +0300

    lesson 3 task 16 done

--- lesson3_task16/Program.cs
+++ lesson3_task16/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int NumberOne(int num, int NumberOfFirstNumbers)
+{
+    while (num / Math.Pow(10, NumberOfFirstNumbers) > 1)
+    {
+        num /= 10;
+    }
+    return num;
+}
+
+int number = int.Parse(Console.ReadLine());
+if (number < 0) number *= -1;
+if (number > 99)
+{
+    int x = number % 100;
+    Console.WriteLine(NumberOne(number, 2));
+    Console.WriteLine(x);
+}
+else if (number > 9)
+{
+    int x = number % 10;
+    Console.WriteLine(NumberOne(number, 1));
+    Console.WriteLine(x);
+}
+else
+{
+    Console.WriteLine("Enter number more 9");
+}
