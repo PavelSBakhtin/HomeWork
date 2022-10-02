79f2d0278d397714551949a5fb3de87d5e71798d
PavelSB <PavelS@mail.ru>
2022-10-03 01:11:10 +0300

    lesson 1 task 2 done

--- lesson1_task2/Program.cs
+++ lesson1_task2/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите два любых целых числа");
-int user_numberA = int.Parse(Console.ReadLine());
-int user_numberB = int.Parse(Console.ReadLine());
+int user_numberA = int.Parse(Console.ReadLine()??"0");
+int user_numberB = int.Parse(Console.ReadLine()??"0");
 
 if (user_numberA > user_numberB)
 {
