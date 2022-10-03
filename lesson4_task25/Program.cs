0e458b9dff37ed1f279a828b4715cd04c553d995
PavelSB <1987PavelS@mail.ru>
2022-10-03 03:19:07 +0300

    lesson 4 task 25 done

---
+++ lesson4_task25/Program.cs
@@ -0,0 +1,27 @@
+﻿int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine()); // конструкция ??"0" нивелирует ошибку NULL
+    return number;
+}
+
+void PrintData(string msg, int value)
+{
+    Console.WriteLine(msg + value);
+}
+
+int PowAtoB(int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
+int numA = ReadData("Enter number A: ");
+int numB = ReadData("Enter number B: ");
+
+int result = PowAtoB(numA, numB);
+PrintData($"{numA} ^ {numB} = ", result);
