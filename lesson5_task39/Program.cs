ec78de37817c90ae39e1c1c2fd48a1c2830ed73f
PavelSB <1987PavelS@mail.ru>
2022-10-03 20:58:38 +0300

    lesson 5 task 39 added

---
+++ lesson5_task39/Program.cs
@@ -0,0 +1,41 @@
+﻿string[,] matrix = {
+    {" "," "," "," "," "," "," "},
+    {" "," "," "," "," "," "," "},
+    {" ","@ "," "," "," "," "," "},
+    {" "," "," "," "," "," "," "},
+    {" "," "," "," "," "," "," "},
+    {" "," "," "," "," "," "," "},
+    {" "," "," "," "," "," "," "}};
+
+int X = 1;
+int Y = 2;
+void WriteArrayMatrix(string[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Game(string[,] matrix, int X, int Y)
+{
+    while (true)
+    {
+        matrix[Y, X] = " ";
+        ConsoleKeyInfo user_keyTab = Console.ReadKey();
+        if (user_keyTab.Key == ConsoleKey.W) Y--;
+        if (user_keyTab.Key == ConsoleKey.S) Y++;
+        if (user_keyTab.Key == ConsoleKey.A) X--;
+        if (user_keyTab.Key == ConsoleKey.D) X++;
+        matrix[Y, X] = "@";
+        Console.Clear();
+        WriteArrayMatrix(matrix);
+    }
+}
+
+WriteArrayMatrix(matrix);
+Game(matrix, X, Y);
