52f67c9acd0ac5abc2ba3f72001cbd2be860bd84
PavelSB <1987PavelS@mail.ru>
2022-10-03 04:38:41 +0300

    lesson 4 task 25* done - home work

---
+++ lesson4_task25!/Program.cs
@@ -0,0 +1,52 @@
+﻿double ReadData(string msg)
+{
+    Console.Write(msg);
+    double number = double.Parse(Console.ReadLine()); // конструкция ??"0" нивелирует ошибку NULL
+    return number;
+}
+
+string ReadSing(string msg)
+{
+    Console.Write(msg);
+    string sing = Console.ReadLine();
+    return sing;
+}
+
+void PrintData(string msg, double value)
+{
+    Console.Write(msg);
+    Console.WriteLine(value);
+}
+
+double Calculator(double a, string sing, double b)
+{
+    double result = 0;
+    if (sing == "+")
+    {
+        result = a + b;
+    }
+    if (sing == "-")
+    {
+        result = a - b;
+    }
+    if (sing == "*")
+    {
+        result = a * b;
+    }
+    if (sing == "/")
+    {
+        result = a / b;
+    }
+    if (sing == "^")
+    {
+        result = Math.Pow(a, b);
+    }
+    return result;
+}
+
+double numA = ReadData("Enter number A: ");
+string sing = ReadSing("Enter operation sing: ");
+double numB = ReadData("Enter number B: ");
+
+double result = Calculator(numA, sing, numB);
+PrintData($"{numA} {sing} {numB} = ", result);
