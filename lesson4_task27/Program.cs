8700e7a53c3f45878d34ba8ffaa2ea2c3194b6c9
PavelSB <1987PavelS@mail.ru>
2022-10-03 03:43:24 +0300

    lesson 4 task 27 done - home work

---
+++ lesson4_task27/Program.cs
@@ -0,0 +1,26 @@
+﻿int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine()); // конструкция ??"0" нивелирует ошибку NULL
+    return number;
+}
+
+void PrintData(string msg, int value)
+{
+    Console.WriteLine(msg + value);
+}
+
+int DigitCount(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + num % 10;
+        num /= 10;
+    }
+    return result;
+}
+
+int num = ReadData("Enter any integer: ");
+int sum = DigitCount(num);
+PrintData($"The sum of the digits in integer {num} = ", sum);
