246fedaeccbfdd1d7fe3767fc07d150684247b4a
PavelSB <1987PavelS@mail.ru>
2022-10-03 03:55:23 +0300

    lesson 4 task 29 done - home work

---
+++ lesson4_task29/Program.cs
@@ -0,0 +1,32 @@
+﻿int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine()); // конструкция ??"0" нивелирует ошибку NULL
+    return number;
+}
+
+int[] Fill1DArray(int length, int lowBord, int highBord)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(lowBord, highBord + 1);
+    }
+    return array;
+}
+
+void Print1DArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1] + "]");
+}
+
+int arrayLength = ReadData("Enter the length of the array: ");
+int min = ReadData("Enter the MIN value of the array: ");
+int max = ReadData("Enter the MAX value of the array: ");
+int[] array = Fill1DArray(arrayLength, min, max);
+Print1DArray(array);
