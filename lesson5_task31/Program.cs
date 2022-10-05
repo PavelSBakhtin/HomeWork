f4c74452a6e4f4b3e2226dcfc9875e9d574ce209
PavelSB <1987PavelS@mail.ru>
2022-10-05 03:25:18 +0300

    lesson 5 task 31 done

---
+++ lesson5_task31/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа по нахождению рандомного слова из списка:
+
+string words = Console.ReadLine() ?? "0";
+string[] nums = words.Split(", ");
+int index = new Random().Next(0,nums.Length);
+Console.WriteLine(nums[index]);
+
+Console.WriteLine(String.Join(" ",nums)); // Эта команда объединит все элементы (после удаления из строки ", ") и выведет через указанный элемент (в данном случае это " ").
