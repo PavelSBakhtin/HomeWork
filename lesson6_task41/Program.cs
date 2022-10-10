a32812d1026862a4e1f4df33f20d90405b109fc8
PavelSB <1987PavelS@mail.ru>
2022-10-10 07:53:51 +0300

    lesson 6 task 41 done - HomeWork

---
+++ lesson6_task41/Program.cs
@@ -0,0 +1,27 @@
+﻿string ReadData(string msg)
+{
+    Console.Write(msg);
+    string numbers = Console.ReadLine() ?? "0"; // конструкция ??"0" нивелирует ошибку NULL
+    return numbers;
+}
+
+// string Numbers(string nums) // не получилось сделать красиво...
+// {
+//     string noSpaces = nums.Replace(" ", "");
+//     string toArray = noSpaces.Replace(",", " "); 
+//     return toArray;
+// }
+
+
+
+int[] array = Array.ConvertAll(ReadData("Enter some numbers separated by spaces: ").Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0) count++;
+}
+
+Console.WriteLine($"Numbers greater than 0: {count}");
+
+
