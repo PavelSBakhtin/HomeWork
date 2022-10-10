76b211f38f70ad7c72d5c97d4b35ef950a831a3a
PavelSB <1987PavelS@mail.ru>
2022-10-11 01:06:04 +0300

    lesson 6 task 41 done

--- lesson6_task41/Program.cs
+++ lesson6_task41/Program.cs
@@ -1,27 +1,34 @@
 ﻿string ReadData(string msg)
 {
     Console.Write(msg);
-    string numbers = Console.ReadLine() ?? "0"; // конструкция ??"0" нивелирует ошибку NULL
+    string numbers = Console.ReadLine() ?? "0";
     return numbers;
 }
 
-// string Numbers(string nums) // не получилось сделать красиво...
-// {
-//     string noSpaces = nums.Replace(" ", "");
-//     string toArray = noSpaces.Replace(",", " "); 
-//     return toArray;
-// }
-
-
-
-int[] array = Array.ConvertAll(ReadData("Enter some numbers separated by spaces: ").Split(), int.Parse);
-int count = 0;
-
-for (int i = 0; i < array.Length; i++)
+void PrintData(string msg, int value)
 {
-    if(array[i] > 0) count++;
+    Console.WriteLine(msg + value);
 }
 
-Console.WriteLine($"Numbers greater than 0: {count}");
+string numbers = ReadData("Enter some numbers: ");
+string[] array = numbers.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+// // // Если в условиях Split убрать создание нового массива и оставить только ','
+// // // - то тогда, при перечислении чисел через пробел, программа выдаст ошибку.
+int sum = 0;
+foreach (var item in array)
+{
+    if (int.Parse(item) > 0) sum++;
+}
+PrintData($"Sum of numbers greater than 0 = ", sum);
 
 
+// // Изначальное решение:
+// int[] array = Array.ConvertAll(ReadData("Enter some numbers separated by spaces: ").Split(), int.Parse);
+// int count = 0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if(array[i] > 0) count++;
+// }
+
+// Console.WriteLine($"Numbers greater than 0: {count}");
