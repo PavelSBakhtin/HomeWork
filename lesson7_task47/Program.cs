1a1ecf59569397b213c863b4c631c32679a972a8
PavelSB <1987PavelS@mail.ru>
2022-10-11 01:18:32 +0300

    lesson 7 task 47 done - HomeWork

--- lesson7_task47/Program.cs
+++ lesson7_task47/Program.cs
@@ -1,10 +1,22 @@
-﻿void FillArray(double[,] matrixNew)
+﻿int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+void FillArray(double[,] matrixNew)
 {
     for (int i = 0; i < matrixNew.GetLength(0); i++)
     {
         for (int j = 0; j < matrixNew.GetLength(1); j++)
         {
             matrixNew[i, j] = Math.Round(new Random().NextDouble() * new Random().Next(-10, 11), 1);
+            // Для преподавателя:
+            // Ничего более интересней конструкции я не придумал,
+            // подозреваю, что существует, но - не додумался.
+            // Технически условие задачи выполнено в полном объеме,
+            // но смущает тот факт, что числа преимущественно от -1 до 1.
         }
     }
 }
@@ -21,7 +33,9 @@ void PrintArray(double[,] matrixNew)
     }
 }
 
-double[,] matrix = new double[3, 4];
+int y = ReadData("Enter the number of lines: "); // for example 3
+int x = ReadData("Enter the number of columns: "); // for example 4
+double[,] matrix = new double[y, x];
 FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
