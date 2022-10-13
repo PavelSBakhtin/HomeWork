4d1ad391b6e98453def4433467a9be5aaa15c374
PavelSB <1987PavelS@mail.ru>
2022-10-13 17:51:19 +0300

    lesson 8 task 53 done

--- lesson8_task53/Program.cs
+++ lesson8_task53/Program.cs
@@ -1,4 +1,8 @@
-﻿int[,] GetArray(int m, int n, int min, int max)
+﻿// 1. пользователь вводит несколько чисел.
+// 2. создаётся некий двумерный массив.
+// 3. программа считает - сколько в массиве встречается чисел, введённых пользователем.
+
+int[,] GetArray(int m, int n, int min, int max) // метод создаёт массив
 {
     int[,] result = new int[m, n];
     for (int i = 0; i < m; i++)
@@ -6,14 +10,16 @@
         for (int j = 0; j < n; j++)
         {
             result[i, j] = new Random().Next(min, max + 1);
-            Console.Write(result[i, j] + "\t");
+            if (j == 0) Console.Write("{0,3}", result[i, j]);
+            else Console.Write("{0,5}", result[i, j]);
+            // Console.Write(result[i, j] + "\t");
         }
         Console.WriteLine();
     }
     return result;
 }
 
-int[] GoodArray(int[,] matrix, int[] array)
+int[] GoodArray(int[,] matrix, int[] array) // метод ищет и считает количество введённых чисел в массиве
 {
     int[] arrOut = new int[array.Length];
     for (int i = 0; i < array.Length; i++)
@@ -29,11 +35,11 @@ int[] GoodArray(int[,] matrix, int[] array)
     return arrOut;
 }
 
-int[] array = Console.ReadLine()!.Split(" ").Select(x => int.Parse(x)).ToArray();
+int[] array = Console.ReadLine()!.Split(" ").Select(x => int.Parse(x)).ToArray(); // ввод чисел
 
-int[] good = GoodArray(GetArray(6, 6, -10, 11), array);
+int[] good = GoodArray(GetArray(6, 6, -10, 11), array); // вызов методов (стр.5, стр.22)
 
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < array.Length; i++) // печать на экран результата
 {
     Console.WriteLine($"Число {array[i]} = {good[i]}");
 }
---
+++ lesson8_task54/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 54: Задайте двумерный массив.
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void PrintData(string msg, string averages)
+{
+    Console.WriteLine(msg + averages);
+}
+
+void FillArray(int[,] matrixNew)
+{
+    for (int i = 0; i < matrixNew.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixNew.GetLength(1); j++)
+        {
+            matrixNew[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrixNew)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrixNew.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixNew.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write($"{matrixNew[i, j]} ");
+            else Console.Write("{0,3}", $"{matrixNew[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+string Average(int[,] getMatrix, int row, int column)
+{
+    double sum = 0;
+    double average = 0;
+    string result = string.Empty;
+    for (int j = 0; j < row; j++)
+    {
+        for (int i = 0; i < column; i++)
+        {
+            sum += getMatrix[i, j];
+        }
+        average = sum / row;
+        average = Math.Round(average, 1);
+        if (j != row - 1) result += $"{average.ToString()}; ";
+        else result += $"{average.ToString()}. ";
+        // result += String.Join("", $"{average.ToString()}; "); - первое решение.
+        average = 0;
+        sum = 0;
+    }
+    return result;
+}
+
+int y = new Random().Next(3, 10);
+int x = new Random().Next(4, 10);
+int[,] matrix = new int[y, x];
+FillArray(matrix);
+PrintArray(matrix);
+PrintData("Average of each column: ", Average(matrix, x, y));
